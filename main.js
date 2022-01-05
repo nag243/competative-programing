47b72bc7edf5e3c2724e80481a3f778e83f0094d
nag243 <nagsall243@gmail.com>
2022-01-05 09:36:48 +0530

    parseint challenge

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ function multiply(x){
     }
 }
  console.log(multiply(2)(3))
+
+ //parse int
+
+ const num = parseInt('7*6', 10);
+ 
+ console.log(num)
+
+ //output = 7
+
+// It parses the string and returns the first integer, hence num will have 7
+
+
