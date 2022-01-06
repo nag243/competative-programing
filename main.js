19c8f12cef84747a2461981f89766e5b96ab7656
nag243 <nagsall243@gmail.com>
2022-01-06 20:13:58 +0530

    Generate random booleana value using Math.random()

--- main.js
+++ main.js
@@ -16,3 +16,15 @@ function multiply(x){
 // It parses the string and returns the first integer, hence num will have 7
 
 
+//Generate random Boolean values by using 𝗠𝗮𝘁𝗵.𝗿𝗮𝗻𝗱𝗼𝗺() function.
+// 𝗠𝗮𝘁𝗵.𝗿𝗮𝗻𝗱𝗼𝗺() generates random floating-point values range between 0 to 1.
+// By checking middle value like 0.5 we can get probability of both true and false.
+// Using <=, >= , < or >
+
+
+function germathrandom(){
+    return Math.random() >= 0.5;
+}
+
+germathrandom()
+
