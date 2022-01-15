8625b78569e715bd1b0632f844d45defa61f0013
nag243 <nagsall243@gmail.com>
2022-01-15 23:34:10 +0530

    compareing number with object reference

--- main.js
+++ main.js
@@ -96,3 +96,18 @@ const todo = {
 false
 !!todo.title
 true
+
+
+
+// 'b' is an object, whereas 'a' and 'c' are of type number.
+// true, false, false
+
+let a  = 3
+let b  = new Number(3)
+let c = 3;
+
+console.log(a == b)
+console.log(a === b)
+console.log(b === c)
+
+
