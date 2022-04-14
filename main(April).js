c0b95dc57e26c357840faa07be37807b77d2d033
Nag <nagsall243@gmail.com>
2022-04-14 19:25:38 +0530

    JS Quiz - 14, JS Quiz - 15

--- main(April).js
+++ main(April).js
@@ -1,7 +1,7 @@
 // JS quiz -  1
 
 console.log(Math.max())
--Infinity
+    - Infinity
 
 console.log(Math.min())
 Infinity
@@ -10,7 +10,7 @@ Infinity
 
 // Many of us have encountered Factorial logic in an interview that requires numerous lines of code, however we can create this code in just one line.Have a look below 👇.
 
-const factorial = (n) => (n <= 1 ? 1 : n* factorial(n - 1))
+const factorial = (n) => (n <= 1 ? 1 : n * factorial(n - 1))
 
 factorial(2)
 2
@@ -23,11 +23,11 @@ factorial(5)
 // type of [ ] is object
 const array = [];
 
-if(!array) {
+if (!array) {
     console.log("I have no values")
 }
 
-if(!array?.length) {
+if (!array?.length) {
     console.log("No, I really have no values");
 }
 // Output:
@@ -36,7 +36,7 @@ if(!array?.length) {
 // JS Quiz - 4
 //  How to get the battery status of user machine's
 
-navigator.getBattery().then((battery) =>{
+navigator.getBattery().then((battery) => {
     console.log(battery)
 })
 
@@ -46,14 +46,14 @@ navigator.getBattery().then((battery) =>{
 
 condition && doAction();
 // Short circuit evalution is a neat trick. if the left side of an AND is aleady false, you don't need to evalute the right side.
-if(condition){
+if (condition) {
     doAction();
 }
 
 // JS Quiz - 6
 
-navigator.geolocation.getCurrentPosition(function getPosition (position) {
-    console.log( position.coords.latitude);
+navigator.geolocation.getCurrentPosition(function getPosition(position) {
+    console.log(position.coords.latitude);
     console.log(position.coords.longitude)
 })
 
@@ -63,12 +63,12 @@ navigator.geolocation.getCurrentPosition(function getPosition (position) {
 
 // JS Quiz - 7
 // Replace with value 0 from index 1 up to but not including 3.
-console.log([1,2,3,4,5].fill(0,1,3))
- [1, 0, 0, 4, 5]
+console.log([1, 2, 3, 4, 5].fill(0, 1, 3))
+[1, 0, 0, 4, 5]
 
 // JS Quiz - 8
 // The .reduce() array method in JavaScript is really powerful - and it's useful to combine multiple elements from an array into a single value
-const prices = [8,41,17,29,33]
+const prices = [8, 41, 17, 29, 33]
 
 const sum = prices.reduce((totalPrice, currentPrice) => {
     return totalPrice += currentPrice
@@ -80,14 +80,14 @@ console.log(sum)
 // JS Quiz - 9
 // delete operator in JavaScript
 const user = {
-    firstName:'Nag',
+    firstName: 'Nag',
     lastName: 'Wick'
 };
 
 delete user.lastName;
 
 console.log(user)
-{firstName: 'Nag'}
+{ firstName: 'Nag' }
 
 //  JS Quiz - 10
 // ***** Regex with Javascript ****
@@ -102,8 +102,8 @@ console.log(result)
 true
 
 //  JS Quiz - 11
-const handleVisabilityChange=()=>{
-    if(document.visibilityState === "hiden") {
+const handleVisabilityChange = () => {
+    if (document.visibilityState === "hiden") {
         console.log("user left the page")
     } else {
         console.log("user opened the page")
@@ -122,9 +122,9 @@ document.addEventListener("visibilitychange", handleVisabilityChange);
 //  ✔️ 𝘐𝘵 𝘪𝘴 𝘦𝘹𝘵𝘳𝘦𝘮𝘦𝘭𝘺 𝘶𝘴𝘦𝘧𝘶𝘭 𝘪𝘯 𝘦𝘷𝘦𝘯𝘵 𝘩𝘢𝘯𝘥𝘭𝘪𝘯𝘨. 
 
 function num(x) {
-    return function(y) {
-        return function(z) {
-            return x*y*z 
+    return function (y) {
+        return function (z) {
+            return x * y * z
         }
     }
 }
@@ -137,12 +137,14 @@ console.log('first')
 
 function second() {
     setTimeout(function () {
-        console.log('second') }, 2000)
+        console.log('second')
+    }, 2000)
 }
 
 function third() {
     setTimeout(function () {
-        console.log('third') }, 1000)
+        console.log('third')
+    }, 1000)
 }
 
 async function start() {
@@ -155,4 +157,50 @@ start()
 // first 
 // third
 // second
+
+// JS Quiz - 14
+// Looking for the output as first, second, third
+console.log('first'); 
+
+function second(){ 
+return new   Promise((resolve,reject)=>{
+ setTimeout(function(){ 
+   console.log('second') },2000)
+})
+} 
+
+function third(){ 
+ setTimeout(function(){ 
+   console.log('third') },1000) 
+} 
+async function start(){ 
+ await second();
+ third(); 
+} 
+start();
+// Output:
+// first
+// second
+
+
+// JS Quiz - 15
+// Every() is a JS function that checks whether all elements pass a certain condition.
+
+
+const arr = [1, 2, 3, 4, 5, 6]
+const even = x => x % 2 == 0;
+
+if (arr.every(even))
+    console.log('Every element is even');
+else
+    console.log('At least one element was odd');
+
+// Output:
+// At least one element was odd
+
+
+
+
+
+
 
