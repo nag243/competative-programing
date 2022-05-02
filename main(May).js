bba6209428ff9ab18d437a984997c171df30d3a2
Nag <nagsall243@gmail.com>
2022-05-02 19:35:08 +0530

    JS Quiz - 2

--- main(May).js
+++ main(May).js
@@ -9,3 +9,20 @@ setTimeout(func, 100)
 func = () => {
     console.log(2)
 }
+
+// JS Quiz - 2 
+//Trucate numbers in JS
+console.log(~~47.11) //47
+
+console.log(~~1.9999) //1
+
+console.log(~~3) //3
+
+// works with 32 bit integer. so if over maximum 32-bit integer
+console.log(2147883647 + 1000)
+2147884647
+
+// all non-number converts tot 0
+console.log(~~[]) //0
+console.log(~~NaN) //0
+console.log(~~null) //0
